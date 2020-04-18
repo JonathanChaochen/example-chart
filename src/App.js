122d0332b67ccfc859e29916d108d0f65083e28d
Jonathan Chen <chaochen42@gmail.com>
2020-04-18 21:11:32 +1200

    Change to NZ Coivd-19 Data Chart
    https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-current-situation/covid-19-current-cases

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Chart from "./Chart/index";
 export default function App() {
   return (
     <ErrorBoundary>
-      <div style={{ height: "1000px" }}>
-        <h1>Echart</h1>
+      <div style={{ height: "900px" }}>
+        <h1>Covid-19 New Zealand</h1>
         <Chart />
       </div>
     </ErrorBoundary>
--- src/Chart/index.js
+++ src/Chart/index.js
@@ -6,17 +6,86 @@ function Chart(props) {
 
   const getOptions = () => {
     const option = {
-      xAxis: {
-        type: "category",
-        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+      dataset: {
+        source: [
+          [
+            "DHB",
+            "Active",
+            "Recovered",
+            "Deceased",
+            "Total",
+            "Change in last 24 hours"
+          ],
+          ["Auckland", 49, 136, 0, 185, 1],
+          ["Bay of Plenty", 13, 32, 0, 45, 1],
+          ["Canterbury", 64, 76, 7, 147, 1],
+          ["Capital and Coast", 42, 49, 2, 93, 1],
+          ["Counties Manukau", 32, 78, 0, 110, 1],
+          ["Hawke's Bay", 21, 20, 0, 41, 0],
+          ["Hutt Valley", 7, 13, 0, 20, 0],
+          ["Lakes", 5, 11, 0, 16, 1],
+          ["Mid Central", 11, 20, 0, 31, 2],
+          ["Nelson Marlborough", 14, 34, 0, 48, 0],
+          ["Northland", 17, 10, 0, 27, 1],
+          ["South Canterbury", 9, 6, 0, 15, 1],
+          ["Southern", 64, 152, 0, 216, 2],
+          ["Tairāwhiti", 3, 1, 0, 4, 0],
+          ["Taranaki", 3, 11, 0, 14, 0],
+          ["Waikato", 98, 84, 1, 183, 0],
+          ["Wairarapa", 0, 8, 0, 8, 0],
+          ["Waitematā", 86, 120, 0, 206, 0],
+          ["West Coast", 2, 2, 1, 5, 0],
+          ["Whanganui", 4, 4, 0, 8, 1]
+        ]
       },
-      yAxis: {
+      title: {
+        text: "Total cases by DHB, 18 April 2020"
+      },
+      legend: {
+        data: ["Active", "Recovered", "Deceased"],
+        bottom: 5
+      },
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "shadow",
+          label: {
+            backgroundColor: "#6a7985"
+          }
+        }
+      },
+      xAxis: {
+        name: "Number of cases",
         type: "value"
       },
+      yAxis: [
+        {
+          type: "category",
+          name: "Distric Health Board"
+        }
+      ],
+      color: ["#e3dc54", "#51ba43", "#cc0000"],
       series: [
         {
-          data: [820, 932, 901, 934, 1290, 1330, 1320],
-          type: "line"
+          name: "Active",
+          type: "bar",
+          encode: { x: "Active", y: "DHB" },
+          stack: "Total",
+          style: {
+            color: "red"
+          }
+        },
+        {
+          name: "Recovered",
+          type: "bar",
+          encode: { x: "Recovered", y: "DHB" },
+          stack: "Total"
+        },
+        {
+          name: "Deceased",
+          type: "bar",
+          encode: { x: "Deceased", y: "DHB" },
+          stack: "Total"
         }
       ]
     };
