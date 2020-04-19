9e22dcd45d08971a5001ec4797e4a446b3d17100
Jonathan Chen <chaochen42@gmail.com>
2020-04-20 10:56:20 +1200

    Adjust charts options

---
+++ src/Chart/sources.js
@@ -0,0 +1,30 @@
+export const cases = [
+  [
+    "DHB",
+    "Active",
+    "Recovered",
+    "Deceased",
+    "Total",
+    "Change in last 24 hours"
+  ],
+  ["Auckland", 49, 136, 0, 185, 1],
+  ["Bay of Plenty", 13, 32, 0, 45, 1],
+  ["Canterbury", 64, 76, 7, 147, 1],
+  ["Capital and Coast", 42, 49, 2, 93, 1],
+  ["Counties Manukau", 32, 78, 0, 110, 1],
+  ["Hawke's Bay", 21, 20, 0, 41, 0],
+  ["Hutt Valley", 7, 13, 0, 20, 0],
+  ["Lakes", 5, 11, 0, 16, 1],
+  ["Mid Central", 11, 20, 0, 31, 2],
+  ["Nelson Marlborough", 14, 34, 0, 48, 0],
+  ["Northland", 17, 10, 0, 27, 1],
+  ["South Canterbury", 9, 6, 0, 15, 1],
+  ["Southern", 64, 152, 0, 216, 2],
+  ["Tairāwhiti", 3, 1, 0, 4, 0],
+  ["Taranaki", 3, 11, 0, 14, 0],
+  ["Waikato", 98, 84, 1, 183, 0],
+  ["Wairarapa", 0, 8, 0, 8, 0],
+  ["Waitematā", 86, 120, 0, 206, 0],
+  ["West Coast", 2, 2, 1, 5, 0],
+  ["Whanganui", 4, 4, 0, 8, 1]
+];
